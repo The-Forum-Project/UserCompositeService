4071d3f238f4a624c26e55362fe0aef6b84e48aa
yiheyoung <47635610+yiheyoung@users.noreply.github.com>
2023-07-20 13:03:46 -0400

    initial version of UserCompositeService

---
+++ src/main/java/com/example/usercompositeservice/UserCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.usercompositeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCompositeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCompositeServiceApplication.class, args);
+	}
+
+}
